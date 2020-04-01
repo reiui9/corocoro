d31b4e33917b2511c369b0e05ec40588419822e5
이유비 <eubnara@naver.com>
2020-04-01 21:21:00 +0900

    [#2] mock up

--- android/app/src/main/java/com/corocoro/CurCovidStatusWidget.java
+++ android/app/src/main/java/com/corocoro/CurCovidStatusWidget.java
@@ -13,10 +13,10 @@ public class CurCovidStatusWidget extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        CharSequence widgetText = context.getString(R.string.appwidget_country);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.cur_covid_status_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+        views.setTextViewText(R.id.appwidget_country, widgetText);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
