ab5b08c92c95e6026c79d3b741a6312c40ddc413
reiui9 <reiui9@naver.com>
2020-04-05 15:54:51 +0900

    add features

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  * @flow
  */
 
-import React, {Component} from 'react';
+import React, {Component, useEffect, useState} from 'react';
 import {
   Container,
   Header,
@@ -20,7 +20,12 @@ import {
 } from 'native-base';
 import {StyleSheet, ScrollView, View, Text} from 'react-native';
 import firestore from '@react-native-firebase/firestore';
-import {BannerAd, BannerAdSize, TestIds} from '@react-native-firebase/admob';
+import {
+  BannerAd,
+  BannerAdSize,
+  InterstitialAd,
+  AdEventType,
+} from '@react-native-firebase/admob';
 import moment from 'moment';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 import {Dimensions, Platform, PixelRatio} from 'react-native';
@@ -53,6 +58,16 @@ if (Platform.OS === 'ios') {
   firebase.initializeApp(iosConfig);
 }
 
+const unitId =
+  Platform.OS === 'ios'
+    ? 'ca-app-pub-4432414803296210/8609448458'
+    : 'ca-app-pub-4432414803296210/2103749852';
+
+const interstitial = InterstitialAd.createForAdRequest(unitId, {
+  requestNonPersonalizedAdsOnly: true,
+  keywords: ['fashion', 'clothing'],
+});
+
 function normalize(size) {
   let newSize = size * scale;
   if (viewportWidth > 450) {
@@ -77,6 +92,7 @@ function numberWithCommas(x) {
 function refresh(self) {
   self.setState({
     arr: [],
+    arr2: [],
     loading: true,
   });
   self.setState({
@@ -95,28 +111,81 @@ function refresh(self) {
     });
 }
 
+function refresh2(self) {
+  self.setState({
+    arr2: [],
+    loading: true,
+    more: true,
+  });
+  return firestore()
+    .collection('dashboard')
+    .doc('total2')
+    .get()
+    .then((doc) => {
+      self.setState({
+        arr2: doc.data().arr,
+        loading: false,
+      });
+    });
+}
+
+// function Ad() {
+//   const [loaded, setLoaded] = useState(false);
+
+//   useEffect(() => {
+//     const eventListener = interstitial.onAdEvent((type) => {
+//       if (type === AdEventType.LOADED) {
+//         setLoaded(true);
+//       }
+//     });
+
+//     // Start loading the interstitial straight away
+//     interstitial.load();
+
+//     // Unsubscribe from events on unmount
+//     return () => {
+//       eventListener();
+//     };
+//   }, []);
+
+//   // No advert ready to show yet
+//   if (!loaded) {
+//     return null;
+//   }
+
+//   return (
+//     <Button
+//       title="Show Interstitial"
+//       onPress={() => {
+//         interstitial.show();
+//       }}
+//     />
+//   );
+// }
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       data: '',
       arr: [],
+      more: true,
+      load: false,
       moment: moment().format('YYYY-MM-DD H:mm:ss'),
     };
   }
   componentDidMount() {
     refresh(this);
+    // interstitial.onAdEvent((type) => {
+    //   if (type === AdEventType.LOADED) {
+    //     this.setState({load: true});
+    //     console.log("loading complete")
+    //   }
+    // });
+    // interstitial.load();
   }
 
   render() {
-    const unitId =
-      Platform.OS === 'ios'
-        ? 'ca-app-pub-4432414803296210/8609448458'
-        : 'ca-app-pub-4432414803296210/2103749852';
-
-    // const bannerWidth = SCREEN_WIDTH*0.8;
-    // var bannerSize = String(bannerWidth) + 'x100';
-
     return (
       <Container>
         <Header
@@ -152,7 +221,7 @@ class App extends Component {
                   <Text style={styles.highlight}>
                     {numberWithCommas(this.state.data.confirmed)}
                     {this.state.data.confirmed_prev && (
-                      <Text style={styles.blue}>
+                      <Text style={[styles.blue, styles.sub]}>
                         {' '}
                         (+
                         {this.state.data.confirmed -
@@ -166,18 +235,26 @@ class App extends Component {
                   <Text style={styles.highlight}>
                     {numberWithCommas(this.state.data.death)}
                     {this.state.data.death_prev && (
-                      <Text style={styles.blue}>
+                      <Text style={[styles.blue, styles.sub]}>
                         {' '}
                         (+{this.state.data.death - this.state.data.death_prev})
                       </Text>
                     )}
+                    <Text style={[{color: 'gray'}, styles.sub]}>
+                      {' '}
+                      {(
+                        (this.state.data.death * 100) /
+                        this.state.data.confirmed
+                      ).toPrecision(2)}
+                      %
+                    </Text>
                   </Text>
                 </Col>
                 <Col style={styles.grid}>
                   <Text style={styles.highlight}>
                     {numberWithCommas(this.state.data.released)}
                     {this.state.data.released_prev && (
-                      <Text style={styles.blue}>
+                      <Text style={[styles.blue, styles.sub]}>
                         {' '}
                         (+
                         {this.state.data.released -
@@ -185,73 +262,203 @@ class App extends Component {
                         )
                       </Text>
                     )}
+                    <Text style={[{color: 'gray'}, styles.sub]}>
+                      {' '}
+                      {(
+                        (this.state.data.released * 100) /
+                        this.state.data.confirmed
+                      ).toPrecision(2)}
+                      %
+                    </Text>
                   </Text>
                 </Col>
               </Grid>
             </View>
+            {this.state.arr &&
+              this.state.arr.map((item, index) => {
+                return (
+                  (!this.state.search ||
+                    item.nation.includes(this.state.search)) && (
+                    <View style={styles.sectionContainer}>
+                      <Text style={styles.sectionTitle}>
+                        {item.number}. {item.flag} {item.nation}
+                      </Text>
+                      <Grid style={styles.girdDescription}>
+                        <Col style={styles.grid}>
+                          <Text style={styles.highlight}>
+                            {numberWithCommas(item.confirmed)}
+                            {item.confirmed_prev && (
+                              <Text style={[styles.blue, styles.sub]}>
+                                {' '}
+                                (+{item.confirmed - item.confirmed_prev})
+                              </Text>
+                            )}
+                          </Text>
+                        </Col>
+                        <Col style={styles.grid}>
+                          <Text style={styles.highlight}>
+                            {numberWithCommas(item.death)}
+                            {item.death_prev && (
+                              <Text style={[styles.blue, styles.sub]}>
+                                {' '}
+                                (+{item.death - item.death_prev})
+                              </Text>
+                            )}
+                            <Text style={[{color: 'gray'}, styles.sub]}>
+                              {' '}
+                              {(
+                                (item.death * 100) /
+                                item.confirmed
+                              ).toPrecision(2)}
+                              %
+                            </Text>
+                          </Text>
+                        </Col>
+                        <Col style={styles.grid}>
+                          <Text style={styles.highlight}>
+                            {numberWithCommas(item.released)}
+                            {item.released_prev && (
+                              <Text style={[styles.blue, styles.sub]}>
+                                {' '}
+                                (+{item.released - item.released_prev})
+                              </Text>
+                            )}
+                            <Text style={[{color: 'gray'}, styles.sub]}>
+                              {' '}
+                              {(
+                                (item.released * 100) /
+                                item.confirmed
+                              ).toPrecision(2)}
+                              %
+                            </Text>
+                          </Text>
+                        </Col>
+                      </Grid>
+                      {(index + 1) % 10 === 0 && (
+                        <BannerAd
+                          unitId={unitId}
+                          size={BannerAdSize.FULL_BANNER}
+                          requestOptions={{
+                            requestNonPersonalizedAdsOnly: true,
+                          }}
+                          onAdLoaded={() => {
+                            console.log('Advert loaded');
+                          }}
+                          onAdFailedToLoad={(error) => {
+                            console.error('Advert failed to load: ', error);
+                          }}
+                        />
+                      )}
+                    </View>
+                  )
+                );
+              })}
+            {this.state.arr2 &&
+              this.state.arr2.map((item, index) => {
+                return (
+                  (!this.state.search ||
+                    item.nation.includes(this.state.search)) && (
+                    <View style={styles.sectionContainer}>
+                      <Text style={styles.sectionTitle}>
+                        {item.number}. {item.flag} {item.nation}
+                      </Text>
+                      <Grid style={styles.girdDescription}>
+                        <Col style={styles.grid}>
+                          <Text style={styles.highlight}>
+                            {numberWithCommas(item.confirmed)}
+                            {item.confirmed_prev && (
+                              <Text style={[styles.blue, styles.sub]}>
+                                {' '}
+                                (+{item.confirmed - item.confirmed_prev})
+                              </Text>
+                            )}
+                          </Text>
+                        </Col>
+                        <Col style={styles.grid}>
+                          <Text style={styles.highlight}>
+                            {numberWithCommas(item.death)}
+                            {item.death_prev && (
+                              <Text style={[styles.blue, styles.sub]}>
+                                {' '}
+                                (+{item.death - item.death_prev})
+                              </Text>
+                            )}
+                            <Text style={[{color: 'gray'}, styles.sub]}>
+                              {' '}
+                              {(
+                                (item.death * 100) /
+                                item.confirmed
+                              ).toPrecision(2)}
+                              %
+                            </Text>
+                          </Text>
+                        </Col>
+                        <Col style={styles.grid}>
+                          <Text style={styles.highlight}>
+                            {numberWithCommas(item.released)}
+                            {item.released_prev && (
+                              <Text style={[styles.blue, styles.sub]}>
+                                {' '}
+                                (+{item.released - item.released_prev})
+                              </Text>
+                            )}
+                            <Text style={[{color: 'gray'}, styles.sub]}>
+                              {' '}
+                              {(
+                                (item.released * 100) /
+                                item.confirmed
+                              ).toPrecision(2)}
+                              %
+                            </Text>
+                          </Text>
+                        </Col>
+                      </Grid>
+                      {(index + 1) % 10 === 0 && (
+                        <BannerAd
+                          unitId={unitId}
+                          size={BannerAdSize.FULL_BANNER}
+                          requestOptions={{
+                            requestNonPersonalizedAdsOnly: true,
+                          }}
+                          onAdLoaded={() => {
+                            console.log('Advert loaded');
+                          }}
+                          onAdFailedToLoad={(error) => {
+                            console.error('Advert failed to load: ', error);
+                          }}
+                        />
+                      )}
+                    </View>
+                  )
+                );
+              })}
+            {this.state.more && !this.state.loading && (
+              <View style={{margin: 10, alignItems: 'center'}}>
+                <Button
+                  rounded
+                  style={{
+                    justifyContent: 'center',
+                    width: 170,
+                    margin: 10,
+                    backgroundColor: 'gray',
+                  }}
+                  onPress={() => {
+                    this.setState({more: false});
+                    refresh2(this);
+                  }}>
+                  <Text
+                    adjustsFontSizeToFit
+                    style={{
+                      fontSize: 15,
+                      fontWeight: 'bold',
+                      color: 'white',
+                    }}>
+                    More
+                  </Text>
+                </Button>
+              </View>
+            )}
             {this.state.loading && <Spinner color="gray" />}
-            {this.state.arr.map((item, index) => {
-              return (
-                (!this.state.search ||
-                  item.nation.includes(this.state.search)) && (
-                  <View style={styles.sectionContainer}>
-                    <Text style={styles.sectionTitle}>
-                      {item.number}. {item.flag} {item.nation}
-                    </Text>
-                    <Grid style={styles.girdDescription}>
-                      <Col style={styles.grid}>
-                        <Text style={styles.highlight}>
-                          {numberWithCommas(item.confirmed)}
-                          {item.confirmed_prev && (
-                            <Text style={styles.blue}>
-                              {' '}
-                              (+{item.confirmed - item.confirmed_prev})
-                            </Text>
-                          )}
-                        </Text>
-                      </Col>
-                      <Col style={styles.grid}>
-                        <Text style={styles.highlight}>
-                          {numberWithCommas(item.death)}
-                          {item.death_prev && (
-                            <Text style={styles.blue}>
-                              {' '}
-                              (+{item.death - item.death_prev})
-                            </Text>
-                          )}
-                        </Text>
-                      </Col>
-                      <Col style={styles.grid}>
-                        <Text style={styles.highlight}>
-                          {numberWithCommas(item.released)}
-                          {item.released_prev && (
-                            <Text style={styles.blue}>
-                              {' '}
-                              (+{item.released - item.released_prev})
-                            </Text>
-                          )}
-                        </Text>
-                      </Col>
-                    </Grid>
-                    {(index + 1) % 10 === 0 && (
-                      <BannerAd
-                        unitId={unitId}
-                        size={BannerAdSize.FULL_BANNER}
-                        requestOptions={{
-                          requestNonPersonalizedAdsOnly: true,
-                        }}
-                        onAdLoaded={() => {
-                          console.log('Advert loaded');
-                        }}
-                        onAdFailedToLoad={(error) => {
-                          console.error('Advert failed to load: ', error);
-                        }}
-                      />
-                    )}
-                  </View>
-                )
-              );
-            })}
             <View style={styles.sectionContainer} />
           </View>
         </ScrollView>
@@ -265,7 +472,12 @@ class App extends Component {
                 this.setState({search: text});
               }}
             />
-            <Button transparent onPress={() => refresh(this)}>
+            <Button
+              transparent
+              onPress={() => {
+                refresh(this);
+                // interstitial.show();
+              }}>
               <Text style={{color: 'black', paddingRight: 15}}>Refresh</Text>
             </Button>
           </Item>
@@ -317,16 +529,22 @@ const styles = StyleSheet.create({
     marginBottom: 2,
   },
   sectionDescription: {
-    fontSize: normalize(13),
+    fontSize: normalize(14),
     fontWeight: '400',
     color: Colors.dark,
   },
   highlight: {
     fontWeight: '700',
-    fontSize: normalize(10),
+    fontSize: normalize(11),
+  },
+  sub: {
+    fontSize: normalize(9),
   },
   blue: {
     color: 'blue',
+  },
+  red: {
+    color: 'red',
   },
   footer: {
     color: Colors.dark,
